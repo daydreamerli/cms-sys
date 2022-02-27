6734b9c2a3fbe907fce84e144bfdd13bcf40b14a
daydreamerli <frankli84@outlook.com>
2022-02-27 18:52:33 +1300

    students related api methods rewrite

--- pages/api/apiRequests.ts
+++ pages/api/apiRequests.ts
@@ -1,25 +1,45 @@
 import { AES } from "crypto-js";
-import { IResponse, LoginRequest, LoginResponse, QueryParams, StudentsRequest, StudentsResponse } from "../../lib/model";
+import { AddStudentRequest, AddStudentResponse, DeleteResponse, IResponse, LoginRequest, LoginResponse, QueryParams, StudentsRequest, StudentsResponse, UpdateStudentRequest, UpdateStudentResponse } from "../../lib/model";
 import { RootPath } from "../../lib/services/api-path";
-import { httpGet, httpPost } from "./httpRequset";
+import { httpDelete, httpGet, httpPost, httpPut } from "./httpRequset";
 
 export async function userLogin(
-    {password, ...rest} : LoginRequest
-): Promise<IResponse<LoginResponse>>{
-    
+    { password, ...rest }: LoginRequest
+): Promise<IResponse<LoginResponse>> {
+
     return httpPost<IResponse<LoginResponse>>(
-        RootPath.login,{
-            ...rest,
-            password: AES.encrypt(password, 'cms').toString(),
-          })
-   
+        RootPath.login, {
+        ...rest,
+        password: AES.encrypt(password, 'cms').toString(),
+    })
+
+}
+
+export async function userLogout(): Promise<IResponse<boolean>>{
+    return httpPost<IResponse<boolean>>(RootPath.logout,{})
 }
 
 export async function getStudentList(
-    req?:StudentsRequest
- ): Promise<IResponse<StudentsResponse>> {
-     return httpGet<IResponse<StudentsResponse>>(
-       RootPath.students,
-       (req as unknown) as QueryParams
-     );
- }
+    req?: StudentsRequest
+): Promise<IResponse<StudentsResponse>> {
+    return httpGet<IResponse<StudentsResponse>>(
+        RootPath.students,
+        (req as unknown) as QueryParams
+    );
+}
+
+export async function getStudentById(id: number): Promise<IResponse<StudentsResponse>> {
+    return httpGet<IResponse<StudentsResponse>>([RootPath.students,id])
+}
+
+export async function addStudent(req:AddStudentRequest): Promise<IResponse<AddStudentResponse>> {
+    return httpPost([RootPath.students],req)
+}
+
+export async function updateStudent(req: UpdateStudentRequest): Promise<IResponse<UpdateStudentResponse>> {
+    return httpPut([RootPath.students],req)
+}
+
+export async function deleteStudent(id: number): Promise<IResponse<DeleteResponse>> {
+    return httpDelete([RootPath.students,id])
+}
